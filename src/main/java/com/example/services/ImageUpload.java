fcb8fb8ee3548acd909c6aba14bd66bc4d52b7c4
Praveen Raj <rpraveen@thoughtworks.com>
2015-04-25 19:48:55 +0530

    calculate near distance.

--- src/main/java/com/example/services/ImageUpload.java
+++ src/main/java/com/example/services/ImageUpload.java
@@ -105,19 +105,6 @@ public class ImageUpload {
     return DriverManager.getConnection(dbUrl, username, password);
   }
 
-
-//  public static double distFrom(double lat1, double lng1, double lat2, double lng2) {
-//    double earthRadius = 6371000; //meters
-//    double dLat = Math.toRadians(lat2-lat1);
-//    double dLng = Math.toRadians(lng2-lng1);
-//    double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-//        Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-//            Math.sin(dLng/2) * Math.sin(dLng/2);
-//    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-//    float dist = (float) (earthRadius * c);
-//
-//    return dist;
-//  }
   public final static double AVERAGE_RADIUS_OF_EARTH = 6371000;
   public int distFrom(double userLat, double userLng,
                                double venueLat, double venueLng) {
@@ -166,7 +153,7 @@ public class ImageUpload {
         );
       }
       result.add("userLat", usrLat)
-          .add("userLon", userLon)
+          .add("userLon", usrLon)
           .add("ImageList", resultList);
       return Response.status(200).entity(result.build().toString()).build();
     } catch (Exception e) {
